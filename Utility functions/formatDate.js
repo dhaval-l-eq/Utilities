6c0781f8a8ff10acbdb398c078860b6b20f9b4c1
dhaval-l-eq <130955904+dhaval-l-eq@users.noreply.github.com>
2023-09-27 10:03:57 +0530

    modified formateDate

--- Utility functions/formatDate.js
+++ Utility functions/formatDate.js
@@ -1,9 +1,38 @@
-import { dateOptions, dateWithTimeOptions } from "../global/config";
+const dateOptions = {
+   year: 'numeric',
+   month: 'short',
+   day: 'numeric',
+};
+
+const monthOptions = {
+   year: 'numeric',
+   month: 'short',
+}
+
+const dateWithTimeOptions = {
+   ...dateOptions,
+   hour: '2-digit',
+   minute: '2-digit',
+};
+
+const timeOptions = {
+   hour: '2-digit',
+   minute: '2-digit',
+};
+
 
 export function formatDate(date) {
    return new Date(date).toLocaleDateString(undefined, dateOptions);
 };
 
+export function formatMonth(date) {
+   return new Date(date).toLocaleDateString(undefined, monthOptions);
+};
+
 export function formatTime(date) {
    return new Date(date).toLocaleDateString(undefined, dateWithTimeOptions);
+}
+
+export function getTime(date) {
+   return new Date(date).toLocaleDateString(undefined, timeOptions);
 }
